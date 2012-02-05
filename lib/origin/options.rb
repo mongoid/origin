4acac84dd1f89ad279a209521fb8647e414a558c
Durran Jordan <durran@gmail.com>
2012-02-05 11:49:09 +0100

    Remove options from origin as underlying drivers will differ

--- lib/origin/options.rb
+++
@@ -1,5 +0,0 @@
-# encoding: utf-8
-module Origin
-  class Options < Hash
-  end
-end
--- lib/origin/queryable.rb
+++ lib/origin/queryable.rb
@@ -1,29 +1,26 @@
 # encoding: utf-8
 require "origin/extensions"
-require "origin/optional"
-require "origin/options"
 require "origin/selection"
 require "origin/selector"
 
 module Origin
   module Queryable
-    include Optional
     include Selection
 
-    attr_reader :options, :selector
+    attr_reader :selector
 
     def ==(other)
       return false unless other.is_a?(Queryable)
-      selector == other.selector && options == other.options
+      selector == other.selector
     end
 
     def initialize
-      @options, @selector = Options.new, Selector.new
+      @selector = Selector.new
       yield(self) if block_given?
     end
 
     def initialize_copy(other)
-      @options, @selector = other.options.__deep_copy__, other.selector.__deep_copy__
+      @selector = other.selector.__deep_copy__
     end
   end
 end
