c7aa034743f7b4c58d2d4e0c303222e32722f36c
Durran Jordan <durran@gmail.com>
2012-02-12 11:03:29 +0100

    Require all in the selectable dir

--- lib/origin/selectable.rb
+++ lib/origin/selectable.rb
@@ -1,30 +1,9 @@
 # encoding: utf-8
-require "origin/selectable/all"
-require "origin/selectable/and"
-require "origin/selectable/between"
-require "origin/selectable/elem_match"
-require "origin/selectable/exists"
-require "origin/selectable/gt"
-require "origin/selectable/gte"
-require "origin/selectable/in"
-require "origin/selectable/key"
-require "origin/selectable/lt"
-require "origin/selectable/lte"
-require "origin/selectable/max_distance"
-require "origin/selectable/mod"
-require "origin/selectable/ne"
-require "origin/selectable/near"
-require "origin/selectable/near_sphere"
-require "origin/selectable/nin"
-require "origin/selectable/nor"
-require "origin/selectable/or"
-require "origin/selectable/size"
-require "origin/selectable/strategies"
-require "origin/selectable/type"
-require "origin/selectable/where"
-require "origin/selectable/within_box"
-require "origin/selectable/within_circle"
-require "origin/selectable/within_spherical_circle"
+
+# Require all files in the selectable directory.
+Dir[ File.dirname(__FILE__) + "/selectable/*.rb" ].each do |file|
+  require file
+end
 
 module Origin
   module Selectable
