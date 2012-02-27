f6b38b52d72e6459711a2846e84dfc8f280a114e
Durran Jordan <durran@gmail.com>
2012-02-27 15:39:18 +0100

    Add api doc into queryable

--- lib/origin/queryable.rb
+++ lib/origin/queryable.rb
@@ -6,21 +6,55 @@ require "origin/selectable"
 require "origin/selector"
 
 module Origin
+
+  # A queryable is any object that needs origin's dsl injected into it to build
+  # MongoDB queries. For example, a Mongoid::Criteria is an Origin::Queryable.
+  #
+  # @example Include queryable functionality.
+  #   class Criteria
+  #     include Origin::Queryable
+  #   end
   module Queryable
     include Selectable
     include Optional
 
+    # Is this queryable equal to another object? Is true if the selector and
+    # options are equal.
+    #
+    # @example Are the objects equal?
+    #   queryable == criteria
+    #
+    # @param [ Object ] other The object to compare against.
+    #
+    # @return [ true, false ] If the objects are equal.
+    #
+    # @since 1.0.0
     def ==(other)
       return false unless other.is_a?(Queryable)
       selector == other.selector &&
         options == other.options
     end
 
+    # Initialize the new queryable. Will yield itself to the block if a block
+    # is provided for objects that need additional behaviour.
+    #
+    # @example Initialize the queryable.
+    #   Origin::Queryable.new
+    #
+    # @since 1.0.0
     def initialize
       @options, @selector = Options.new, Selector.new
       yield(self) if block_given?
     end
 
+    # Handle the creation of a copy via #clone or #dup.
+    #
+    # @example Handle copy initialization.
+    #   queryable.initialize_copy(criteria)
+    #
+    # @param [ Queryable ] other The original copy.
+    #
+    # @since 1.0.0
     def initialize_copy(other)
       @options = other.options.__deep_copy__
       @selector = other.selector.__deep_copy__
