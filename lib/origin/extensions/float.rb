aa3888258aba756134514c7b1e913b31a3bbbe17
Durran Jordan <durran@gmail.com>
2012-02-27 21:18:48 +0100

    Update float extension doc

--- lib/origin/extensions/float.rb
+++ lib/origin/extensions/float.rb
@@ -1,9 +1,21 @@
 # encoding: utf-8
 module Origin
   module Extensions
+
+    # This module contains behaviour extensions for floats.
     module Float
       module ClassMethods
 
+        # Evolve the object into a float for selection.
+        #
+        # @example Evolve the object to a float.
+        #   Float.evolve("2.34")
+        #
+        # @param [ Object ] object The object to evolve.
+        #
+        # @return [ Float ] The evolved float.
+        #
+        # @since 1.0.0
         def evolve(object)
           __numeric__(object) rescue object
         end
