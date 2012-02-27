6cf9afdc0243730fa8e44d3b6cdf363c1cc900e9
Durran Jordan <durran@gmail.com>
2012-02-27 21:14:05 +0100

    Update big decimal extension doc

--- lib/origin/extensions/big_decimal.rb
+++ lib/origin/extensions/big_decimal.rb
@@ -3,9 +3,23 @@ require "bigdecimal"
 
 module Origin
   module Extensions
+
+    # The big decimal module adds custom behaviour for Origin onto the
+    # BigDecimal class.
     module BigDecimal
       module ClassMethods
 
+        # Evolves the big decimal into a MongoDB friendly value - in this case
+        # a string.
+        #
+        # @example Evolve the big decimal
+        #   BigDecimal.evolve(decimal)
+        #
+        # @param [ BigDecimal ] object The object to convert.
+        #
+        # @return [ String ] The big decimal as a string.
+        #
+        # @since 1.0.0
         def evolve(object)
           return object unless object
           object.to_s
