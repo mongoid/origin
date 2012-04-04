3d375c4dbbcc7a323ec749170e331fa37f8f2014
Durran Jordan <durran@gmail.com>
2012-04-04 14:49:57 +0200

    Allow mongo or moped syntax in all sorting. Closes #33

--- spec/origin/optional_spec.rb
+++ spec/origin/optional_spec.rb
@@ -1104,6 +1104,322 @@ describe Origin::Optional do
         end
       end
     end
+
+    context "when using the mongo driver syntax" do
+
+      let(:query) do
+        Origin::Query.new({}, {}, :mongo)
+      end
+
+      context "when provided a hash" do
+
+        context "when the hash has integer values" do
+
+          let(:selection) do
+            query.order_by(field_one: 1, field_two: -1)
+          end
+
+          it "adds the sorting criteria" do
+            selection.options.should eq(
+              { sort: [[ :field_one, 1 ], [ :field_two, -1 ]]}
+            )
+          end
+
+          it "returns a cloned query" do
+            selection.should_not equal(query)
+          end
+        end
+
+        context "when the hash has symbol values" do
+
+          let(:selection) do
+            query.order_by(field_one: :asc, field_two: :desc)
+          end
+
+          it "adds the sorting criteria" do
+            selection.options.should eq(
+              { sort: [[ :field_one, 1 ], [ :field_two, -1 ]]}
+            )
+          end
+
+          it "returns a cloned query" do
+            selection.should_not equal(query)
+          end
+        end
+
+        context "when the hash has string values" do
+
+          let(:selection) do
+            query.order_by(field_one: "asc", field_two: "desc")
+          end
+
+          it "adds the sorting criteria" do
+            selection.options.should eq(
+              { sort: [[ :field_one, 1 ], [ :field_two, -1 ]]}
+            )
+          end
+
+          it "returns a cloned query" do
+            selection.should_not equal(query)
+          end
+        end
+      end
+
+      context "when provided an array" do
+
+        context "when the array is multi-dimensional" do
+
+          context "when the arrays have integer values" do
+
+            let(:selection) do
+              query.order_by([[ :field_one, 1 ],[ :field_two, -1 ]])
+            end
+
+            it "adds the sorting criteria" do
+              selection.options.should eq(
+                { sort: [[ :field_one, 1 ], [ :field_two, -1 ]]}
+              )
+            end
+
+            it "returns a cloned query" do
+              selection.should_not equal(query)
+            end
+          end
+
+          context "when the arrays have symbol values" do
+
+            let(:selection) do
+              query.order_by([[ :field_one, :asc ],[ :field_two, :desc ]])
+            end
+
+            it "adds the sorting criteria" do
+              selection.options.should eq(
+                { sort: [[ :field_one, 1 ], [ :field_two, -1 ]]}
+              )
+            end
+
+            it "returns a cloned query" do
+              selection.should_not equal(query)
+            end
+          end
+
+          context "when the arrays have string values" do
+
+            let(:selection) do
+              query.order_by([[ :field_one, "asc" ],[ :field_two, "desc" ]])
+            end
+
+            it "adds the sorting criteria" do
+              selection.options.should eq(
+                { sort: [[ :field_one, 1 ], [ :field_two, -1 ]]}
+              )
+            end
+
+            it "returns a cloned query" do
+              selection.should_not equal(query)
+            end
+          end
+        end
+
+        context "when the array is selectable keys" do
+
+          let(:selection) do
+            query.order_by([ :field_one.asc, :field_two.desc ])
+          end
+
+          it "adds the sorting criteria" do
+            selection.options.should eq(
+              { sort: [[ :field_one, 1 ], [ :field_two, -1 ]]}
+            )
+          end
+
+          it "returns a cloned query" do
+            selection.should_not equal(query)
+          end
+        end
+      end
+
+      context "when provided values" do
+
+        context "when the values are arrays" do
+
+          context "when the values have integer directions" do
+
+            let(:selection) do
+              query.order_by([ :field_one, 1 ],[ :field_two, -1 ])
+            end
+
+            it "adds the sorting criteria" do
+              selection.options.should eq(
+                { sort: [[ :field_one, 1 ], [ :field_two, -1 ]]}
+              )
+            end
+
+            it "returns a cloned query" do
+              selection.should_not equal(query)
+            end
+          end
+
+          context "when the values have symbol directions" do
+
+            let(:selection) do
+              query.order_by([ :field_one, :asc ],[ :field_two, :desc ])
+            end
+
+            it "adds the sorting criteria" do
+              selection.options.should eq(
+                { sort: [[ :field_one, 1 ], [ :field_two, -1 ]]}
+              )
+            end
+
+            it "returns a cloned query" do
+              selection.should_not equal(query)
+            end
+          end
+
+          context "when the values have string directions" do
+
+            let(:selection) do
+              query.order_by([ :field_one, "asc" ],[ :field_two, "desc" ])
+            end
+
+            it "adds the sorting criteria" do
+              selection.options.should eq(
+                { sort: [[ :field_one, 1 ], [ :field_two, -1 ]]}
+              )
+            end
+
+            it "returns a cloned query" do
+              selection.should_not equal(query)
+            end
+          end
+        end
+
+        context "when the values are selectable keys" do
+
+          let(:selection) do
+            query.order_by(:field_one.asc, :field_two.desc)
+          end
+
+          it "adds the sorting criteria" do
+            selection.options.should eq(
+              { sort: [[ :field_one, 1 ], [ :field_two, -1 ]]}
+            )
+          end
+
+          it "returns a cloned query" do
+            selection.should_not equal(query)
+          end
+        end
+      end
+
+      context "when provided a string" do
+
+        context "when the direction is lowercase" do
+
+          context "when abbreviated" do
+
+            let(:selection) do
+              query.order_by("field_one asc, field_two desc")
+            end
+
+            it "adds the sorting criteria" do
+              selection.options.should eq(
+                { sort: [[ :field_one, 1 ], [ :field_two, -1 ]]}
+              )
+            end
+
+            it "returns a cloned query" do
+              selection.should_not equal(query)
+            end
+          end
+
+          context "when spelled out" do
+
+            let(:selection) do
+              query.order_by("field_one ascending, field_two descending")
+            end
+
+            it "adds the sorting criteria" do
+              selection.options.should eq(
+                { sort: [[ :field_one, 1 ], [ :field_two, -1 ]]}
+              )
+            end
+
+            it "returns a cloned query" do
+              selection.should_not equal(query)
+            end
+          end
+        end
+
+        context "when the direction is uppercase" do
+
+          context "when abbreviated" do
+
+            let(:selection) do
+              query.order_by("field_one ASC, field_two DESC")
+            end
+
+            it "adds the sorting criteria" do
+              selection.options.should eq(
+                { sort: [[ :field_one, 1 ], [ :field_two, -1 ]]}
+              )
+            end
+
+            it "returns a cloned query" do
+              selection.should_not equal(query)
+            end
+          end
+
+          context "when spelled out" do
+
+            let(:selection) do
+              query.order_by("field_one ASCENDING, field_two DESCENDING")
+            end
+
+            it "adds the sorting criteria" do
+              selection.options.should eq(
+                { sort: [[ :field_one, 1 ], [ :field_two, -1 ]]}
+              )
+            end
+
+            it "returns a cloned query" do
+              selection.should_not equal(query)
+            end
+          end
+        end
+      end
+
+      context "when provided no options" do
+
+        let(:selection) do
+          query.order_by
+        end
+
+        it "returns the query" do
+          selection.should eq(query)
+        end
+
+        it "returns a cloned query" do
+          selection.should_not equal(query)
+        end
+      end
+
+      context "when provided nil" do
+
+        let(:selection) do
+          query.order_by(nil)
+        end
+
+        it "returns the query" do
+          selection.should eq(query)
+        end
+
+        it "returns a cloned query" do
+          selection.should_not equal(query)
+        end
+      end
+    end
   end
 
   [ :skip, :offset ].each do |method|
