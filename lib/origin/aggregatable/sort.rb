322a24b6c34217d082a1b6b0ffb23c9c050c7348
Rodrigo Saito <rodrigo.saito@gmail.com>
2012-11-24 09:47:49 +0100

    Implemented some different ways to use sort

--- lib/origin/aggregatable/sort.rb
+++ lib/origin/aggregatable/sort.rb
@@ -8,6 +8,54 @@ module Origin
         "$sort"
       end
 
+      def initialize(*args)
+        @operator = with_hash(*args) || with_array(*args) || with_string(*args) || with_symbol(*args)
+      end
+
+      private
+      def with_hash(*args)
+        return unless args.first.is_a?(Hash)
+
+        evolve_hash args.first
+      end
+
+      def with_array(*args)
+        return unless args.first.is_a?(Array)
+
+        {}.tap do |operator|
+          args.first.each do |f|
+            operator[f.to_s] = 1
+          end
+        end
+      end
+
+      def with_string(*args)
+        return unless args.first.is_a?(String)
+
+        { args.first => 1 }
+      end
+
+      def with_symbol(*args)
+        return unless args.first.is_a?(Symbol)
+
+        { args.first.to_s => 1 }
+      end
+
+      def evolve_hash(hash)
+        {}.tap do |h|
+          hash.each_pair do |k,v|
+            h[k.to_s] = evolve_value(v)
+          end
+        end
+      end
+
+      def evolve_value(value)
+        return value if value.is_a?(Fixnum)
+
+        return 1 if value == :asc
+        return -1 if value == :desc
+      end
+
     end
 
   end
--- spec/origin/aggregatable/sort_spec.rb
+++ spec/origin/aggregatable/sort_spec.rb
@@ -8,12 +8,74 @@ describe Origin::Aggregatable::Sort do
 
   describe "#initialize" do
 
-    let(:subject) { described_class.new( "age" => -1, "post" => 1 ).operator }
+    let(:subject) { described_class.new(params).operator }
 
-    it { should eq( { "age" => -1, "post" => 1} ) }
+    context "when a hash is provided" do
+
+      let(:expected) { { "age" => -1, "post" => 1} }
+
+      context "with integer as value" do
+
+        let(:params) { { "age" => -1, "post" => 1 } }
+
+        it { should eq expected }
+
+      end
+
+      context "with symbol as values" do
+
+        let(:params) { { "age" => :desc, "post" => :asc } }
+
+        it { should eq expected }
+
+      end
+
+      context "with symbol as keys" do
+
+        let(:params) { { age: :desc, post: :asc } }
+
+        it { should eq expected }
+
+      end
+
+    end
+
+    context "when a list is provided" do
+
+      context "with strings" do
+
+        let(:params) { [ "age", "post" ] }
+
+        it { should eq( { "age" => 1, "post" => 1} ) }
+
+      end
+
+      context "with symbols" do
+
+        let(:params) { [ :age, :post  ] }
+
+        it { should eq( { "age" => 1, "post" => 1} ) }
+
+      end
+
+    end
+
+    context "when a string is provided" do
+
+      let(:params) { "age" }
+
+      it { should eq( { "age" => 1 } ) }
+
+    end
+
+    context "when a symbol is provided" do
+
+      let(:params) { :age }
+
+      it { should eq( { "age" => 1 } ) }
+
+    end
 
   end
-
-
 
 end
