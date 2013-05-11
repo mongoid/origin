f84fb52e4823f2edad62bdcfb023fab5755785fe
Durran Jordan <durran@gmail.com>
2013-05-11 17:32:27 +0200

    Add pipeline initializer spec

--- spec/origin/pipeline_spec.rb
+++ spec/origin/pipeline_spec.rb
@@ -2,4 +2,32 @@ require "spec_helper"
 
 describe Origin::Pipeline do
 
+  describe "#initialize" do
+
+    context "when provided aliases" do
+
+      let(:aliases) do
+        { "id" => "_id" }
+      end
+
+      let(:pipeline) do
+        described_class.new(aliases)
+      end
+
+      it "sets the aliases" do
+        expect(pipeline.aliases).to eq(aliases)
+      end
+    end
+
+    context "when not provided aliases" do
+
+      let(:pipeline) do
+        described_class.new
+      end
+
+      it "sets the aliases to an empty hash" do
+        expect(pipeline.aliases).to be_empty
+      end
+    end
+  end
 end
