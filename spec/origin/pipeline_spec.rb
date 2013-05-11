95f974db6485e09c13386bb1a5d7d5ecbe4abb9a
Durran Jordan <durran@gmail.com>
2013-05-11 17:34:36 +0200

    Add deep copy spec on pipeline

--- spec/origin/pipeline_spec.rb
+++ spec/origin/pipeline_spec.rb
@@ -2,6 +2,33 @@ require "spec_helper"
 
 describe Origin::Pipeline do
 
+  describe "#__deep_copy" do
+
+    let(:project) do
+      { "$project" => { "name" => 1 }}
+    end
+
+    let(:pipeline) do
+      described_class.new
+    end
+
+    before do
+      pipeline.push(project)
+    end
+
+    let(:copied) do
+      pipeline.__deep_copy__
+    end
+
+    it "clones all the objects in the pipeline" do
+      expect(copied.first).to_not equal(project)
+    end
+
+    it "clones the pipeline" do
+      expect(copied).to_not equal(pipeline)
+    end
+  end
+
   describe "#initialize" do
 
     context "when provided aliases" do
